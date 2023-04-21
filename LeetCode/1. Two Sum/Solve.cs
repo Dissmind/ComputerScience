e8d814e59e42e72ebd6d0071dbb8290434217cb6
DISSMIND\DissM <dissmind@gmail.com>
2023-04-21 14:15:48 +0200

    [ADD]: leetcode folder

---
+++ LeetCode/1. Two Sum/Solve.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+
+namespace LeetCodeAndCodewars.LeetCode.N1TwoSum;
+
+public class Solve
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        List<int> list = new List<int>()
+        {
+            nums[0]
+        };
+
+        int[] result = null;
+        
+        for (int i = 1; i < nums.Length; i++)
+        {
+            int needNumber = target - nums[i];
+            
+            if (list.Contains(needNumber))
+            {
+                int needNumbersIndex = list.FindIndex(i => i.Equals(needNumber));
+                
+                result = new int[2]
+                {
+                    needNumbersIndex,
+                    i
+                };             
+                break;    
+            }
+
+            list.Add(nums[i]);
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode/1. Two Sum/Test.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace LeetCodeAndCodewars.LeetCode.N1TwoSum;
+
+public class Test
+{
+    [Test]
+    public void TestExample()
+    {
+        var obj = new Solve();
+        Assert.AreEqual(new int[] {0, 1}, obj.TwoSum(new int[] {2, 7, 11, 15}, 9), "Case 1");
+        Assert.AreEqual(new int[] {1, 2}, obj.TwoSum(new int[] {3, 2, 4}, 6), "Case 2");
+        Assert.AreEqual(new int[] {0, 1}, obj.TwoSum(new int[] {3, 3}, 6), "Case 3");
+    }
+}
