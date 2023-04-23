f381265937acf5b35387965992745386f9d9403e
DISSMIND\DissM <dissmind@gmail.com>
2023-04-23 17:09:57 +0200

    [ADD](Leetcode): 14. Longest Common Prefix

--- LeetCode/14. Longest Common Prefix/Solve.cs
+++ LeetCode/14. Longest Common Prefix/Solve.cs
@@ -5,18 +5,26 @@ public class Solve
     public string LongestCommonPrefix(string[] strs)
     {
         string prefix = "";
-        
-        for (int i = 0; i < strs[0].Length; i++)
+
+        try
         {
-            if (strs[0][i] == strs[1][i] && strs[0][i] == strs[2][i])
+            for (int i = 0; i < strs[0].Length; i++)
             {
-                prefix += strs[0][i];
-            }
-            else
-            {
-                break;
+                if (strs[0][i] == strs[1][i] && strs[0][i] == strs[2][i])
+                {
+                    prefix += strs[0][i];
+                }
+                else
+                {
+                    break;
+                }
             }
         }
+        catch (IndexOutOfRangeException e)
+        {
+            // prefix is done
+        }
+        
         
         return prefix;
     }
