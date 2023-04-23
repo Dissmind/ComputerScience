d9554ab757476c8096f3f6126664dd181b5a578d
DISSMIND\DissM <dissmind@gmail.com>
2023-04-23 16:19:49 +0200

    [ADD](Leetcode): 26. Remove dublicates from sorted list

---
+++ LeetCode/2. Add Two Numbers/Solve.cs
@@ -0,0 +1,131 @@
+﻿using System.Net;
+
+namespace LeetCodeAndCodewars.LeetCode.N2AddTwoNumbers;
+
+public class Solve
+{
+    public class ListNode : IComparable<ListNode> {
+        public ListNode(int val=0, ListNode next=null) {
+            this.val = val;
+            this.next = next;
+        }
+        
+        
+        public int val;
+        public ListNode next;
+
+
+        public static ListNode ToListNode(List<int> list)
+        {
+            if (list.Count == 0)
+            {
+                return new ListNode();
+            }
+
+            if (list.Count < 2)
+            {
+                return new ListNode(list[0]);
+            }
+            
+            var headListNode = new ListNode(list[0]);
+            
+            var tailListNode = headListNode;
+            headListNode.next = tailListNode;
+
+            for (int i = 1; i < list.Count; i++)
+            {
+                tailListNode.next = new ListNode(list[i]);
+                tailListNode = tailListNode.next;
+            }
+
+            return headListNode;
+        }
+
+        public int CompareTo(ListNode? other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        int numberL1 = GetNumberRevertedList(l1);
+        int numberL2 = GetNumberRevertedList(l2);
+
+        int sum = numberL1 + numberL2;
+
+        string sumReverse = IntReverse(sum);
+
+        ListNode result = CreateNewRevertedListNode(sumReverse);
+        
+        return result;
+    }
+
+
+    private ListNode CreateNewRevertedListNode(string value)
+    {
+        var headListNode = new ListNode(Convert.ToInt32(value[0].ToString()));
+        
+        var tailListNode = headListNode;
+        headListNode.next = tailListNode;
+        
+        for (int i = 1; i < value.Length; i++)
+        {
+            tailListNode.next = new ListNode(Convert.ToInt32(value[i].ToString()));
+            tailListNode = tailListNode.next;
+        }
+
+        return headListNode;
+    }
+
+
+    private string IntReverse(int value)
+    {
+        char[] valueArray = value.ToString().ToCharArray();
+
+        string valueStr = "";
+
+        for (int i = valueArray.Length - 1; i >= 0; i--)
+        {
+            valueStr += valueArray[i];
+        }
+        
+        return valueStr;
+    }
+
+
+    public string StringReverse(string value)
+    {
+        char[] charArray = value.ToCharArray(); ;
+        Array.Reverse(charArray);
+        
+        string result = new string(charArray);
+        
+        return result;
+    }
+    
+    private int GetNumberRevertedList(ListNode node)
+    {
+        string str = "";
+
+        if (node.next == null)
+        {
+            str = node.val.ToString();
+            return Convert.ToInt32(str);
+        }
+        
+        while (node.next != null)
+        {
+            str += node.val;
+            node = node.next;
+        }
+        str += node.val;
+
+        str = StringReverse(str);
+        int number = Convert.ToInt32(str);
+
+        return number;
+    }
+}
---
+++ LeetCode/2. Add Two Numbers/Test.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+
+namespace LeetCodeAndCodewars.LeetCode.N2AddTwoNumbers;
+
+public class Tests
+{
+    [Test]
+    public void TestExample()
+    {
+        var obj = new Solve();
+        
+        // Assert.AreEqual
+        // (
+        //     CreateListNode(new List<int>() {7, 0, 8}), 
+        //     obj.AddTwoNumbers
+        //     (
+        //         CreateListNode(new List<int>() {2, 4, 3}),
+        //         CreateListNode(new List<int>() {5, 6, 4})
+        //     ), 
+        //     "Example 1"
+        // );
+        //
+        // Assert.AreEqual
+        // (
+        // CreateListNode(new List<int>() {0}), 
+        // obj.AddTwoNumbers
+        // (
+        //     CreateListNode(new List<int>() {0}),
+        //     CreateListNode(new List<int>() {0})
+        // ), 
+        //     "Example 2"
+        // );
+
+        
+        
+        Assert.AreSame
+        (
+            CreateListNode(new List<int>() {8, 9, 9, 9, 0, 0, 0, 1}), 
+            obj.AddTwoNumbers
+            (
+                CreateListNode(new List<int>() {9, 9, 9, 9, 9, 9, 9}),
+                CreateListNode(new List<int>() {9, 9, 9, 9})
+            ), 
+            "Example 3"
+        );
+    }
+
+
+    private Solve.ListNode CreateListNode(List<int> l) => Solve.ListNode.ToListNode(new List<int>() { 1, 2, 3 });
+}
