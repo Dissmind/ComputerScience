2cd3cd32aa840f0982a275fe492204c83b360ae4
DISSMIND\DissM <dissmind@gmail.com>
2023-04-25 12:22:30 +0200

    [ADD](Leetcode): 724. Find Pivot Index

---
+++ LeetCode/724. Find Pivot Index/Solve.cs
@@ -0,0 +1,29 @@
+﻿namespace Leetcode.N724FindPivotIndex;
+
+public class Solve
+{
+    public int PivotIndex(int[] nums)
+    {
+        int sum = 0;
+        
+        for (int i = 0; i < nums.Length; i++)
+        {
+            sum += nums[i];
+        }
+
+
+        int leftSum = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (sum - nums[i] == leftSum * 2)
+            {
+                return i;
+            }
+
+            leftSum += nums[i];
+        }
+        
+        return -1;
+    }
+}
---
+++ LeetCode/724. Find Pivot Index/Test.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Leetcode.N724FindPivotIndex;
+
+public class Test
+{
+    [Test]
+    public void TestExample()
+    {
+        var obj = new Solve();
+        Assert.AreEqual(3, obj.PivotIndex(new int[] {1,7,3,6,5,6}), "Example 1");
+        Assert.AreEqual(-1, obj.PivotIndex(new int[] {1,2,3}), "Example 2");
+        Assert.AreEqual(0, obj.PivotIndex(new int[] {2,1,-1}), "Example 3");
+    }
+}
