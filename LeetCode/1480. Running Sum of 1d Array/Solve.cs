0409b92346d5cb5be4c8388aef9fd9935bc1b898
DISSMIND\DissM <dissmind@gmail.com>
2023-04-25 12:23:00 +0200

    [ADD](Leetcode): 1480. Running Sum of 1d Array

---
+++ LeetCode/1480. Running Sum of 1d Array/Solve.cs
@@ -0,0 +1,19 @@
+﻿namespace Leetcode.N1480RunningSumOf1DArray;
+
+public class Solve
+{
+    public int[] RunningSum(int[] nums)
+    {
+        int[] result = new int[nums.Length];
+        
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                result[i] += nums[j];
+            }
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode/1480. Running Sum of 1d Array/Test.cs
@@ -0,0 +1,14 @@
+﻿using Leetcode.N1480RunningSumOf1DArray;
+using NUnit.Framework;
+
+public class Test
+{
+    [Test]
+    public void TestExample()
+    {
+        var obj = new Solve();
+        Assert.AreEqual(new int[] {1,3,6,10}, obj.RunningSum(new int[] {1,2,3,4}), "Test example 1");
+        Assert.AreEqual(new int[] {1,2,3,4,5}, obj.RunningSum(new int[] {1,1,1,1,1}), "Test example 2");
+        Assert.AreEqual(new int[] {3,4,6,16,17}, obj.RunningSum(new int[] {3,1,2,10,1}), "Test example 3");
+    }
+}
