d1a863078828e158eec6e7292a9f8f3dfbb0a075
DISSMIND\DissM <dissmind@gmail.com>
2023-04-29 12:11:57 +0200

    [ADD](Leetcode): 121. Best Time to Buy Sell Stock (base tests - complete, but finally tests - returned error of "Time limit exceeded")

---
+++ LeetCode/121. Best Time to Buy Sell Stock/Solve.cs
@@ -0,0 +1,48 @@
+﻿namespace Leetcode.N121BestTimeToBuySellStock;
+
+public class Solve
+{
+    public int MaxProfit(int[] prices)
+    {
+        int maxProfit = 0;
+        
+        for (int i = 0; i < prices.Length - 1; i++)
+        {
+            int costPrice = prices[i];
+            int maxSellPrice = MaxArray(i + 1, prices);
+
+            int profit = maxSellPrice - costPrice;
+
+            if (maxProfit < profit)
+            {
+                maxProfit = profit;
+            }
+        }
+
+        return maxProfit;
+    }
+
+
+    private int indexMaxValue = 0;
+    
+    private int MaxArray(int indexStart, int[] array)
+    {
+        int max = array[indexStart];
+        int maxValuesIndex = indexStart;
+
+        indexStart += 1;
+        indexStart = (indexMaxValue > indexStart) ? indexMaxValue : indexStart;
+        
+        for (int i = indexStart; i < array.Length; i++)
+        {
+            if (max < array[i])
+            {
+                maxValuesIndex = i;
+                max = array[i];
+            }
+        }
+
+        this.indexMaxValue = maxValuesIndex;
+        return max;
+    }
+}
---
+++ LeetCode/121. Best Time to Buy Sell Stock/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Leetcode.N121BestTimeToBuySellStock;
+
+public class Test
+{
+    [Test]
+    public void ExampleTest()
+    {
+        var obj = new Solve();
+        Assert.AreEqual(5, obj.MaxProfit(new int[] {7,1,5,3,6,4}), "Example 1");
+        Assert.AreEqual(0, obj.MaxProfit(new int[] {7,6,4,3,1}), "Example 2");
+    }
+}
