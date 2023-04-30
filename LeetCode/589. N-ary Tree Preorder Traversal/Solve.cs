abd2b6e693128d63dcedb28a872215ceb25f9c4a
DISSMIND\DissM <dissmind@gmail.com>
2023-04-30 22:51:41 +0200

    [ADD](Leetcode): 589. N-ary Tree Preorder Traversal (tests implementation not correct, but leetcode solved)

---
+++ LeetCode/589. N-ary Tree Preorder Traversal/Solve.cs
@@ -0,0 +1,72 @@
+﻿namespace Leetcode.N589NaryTreePreorderTraversal;
+
+public class Solve
+{
+    public class Node {
+        public int val;
+        public IList<Node> children;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val,IList<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+    
+    
+    public IList<int> Preorder(Node root)
+    {
+        List<int> result = new List<int>();
+        if(root==null) return result;
+
+        Stack<Node> st = new();
+
+        st.Push(root);
+        while(st.Count>0)
+        {
+            Node top = st.Pop();
+            result.Add(top.val);
+
+            for(int i = top.children.Count-1;i>=0;i--)
+            {
+                st.Push(top.children[i]);
+            }
+        }
+
+        return result;
+
+
+
+        // var result = new List<int>();
+        //
+        // if (root == null)
+        // {
+        //     return result;
+        // }
+        //
+        // Stack<Node> stack = new Stack<Node>();
+        // stack.Push(root);
+        //
+        // while (stack.Count > 0)
+        // {
+        //     Node node = stack.Pop();
+        //     
+        //     result.Add(node.val);
+        //
+        //     if (node.children != null)
+        //     {
+        //         for (int i = node.children.Count - 1; i >= 0; i--)
+        //         {
+        //             stack.Push(node.children[i]);
+        //         }
+        //     }
+        // }
+        //
+        // return result;
+    }
+}
---
+++ LeetCode/589. N-ary Tree Preorder Traversal/Test.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+
+namespace Leetcode.N589NaryTreePreorderTraversal;
+
+public class Test
+{
+    private readonly Solve solve = new Solve();
+    
+    
+    
+    [Test]
+    public void ExampleTest()
+    {
+        var tree1 = new Solve.Node(1, new List<Solve.Node>()
+        {
+            new Solve.Node(2),
+            new Solve.Node(3, new List<Solve.Node>()
+            {
+                new Solve.Node(5),
+                new Solve.Node(6)
+            }),
+            new Solve.Node(4),
+        });
+        Assert.AreEqual(new List<int>() {1,3,5,6,2,4}, solve.Preorder(tree1).ToList(), "Example 1");
+
+        var tree2 = new Solve.Node(1, new List<Solve.Node>()
+        {
+            new Solve.Node(3, new List<Solve.Node>()
+            {
+                new Solve.Node(6),
+                new Solve.Node(7, new List<Solve.Node>()
+                {
+                    new Solve.Node(11, new List<Solve.Node>()
+                    {
+                        new Solve.Node(14)
+                    })
+                })
+            }),
+            new Solve.Node(2),
+            new Solve.Node(4, new List<Solve.Node>()
+            {
+                new Solve.Node(8, new List<Solve.Node>()
+                {
+                    new Solve.Node(12)
+                })
+            }),
+            new Solve.Node(5, new List<Solve.Node>()
+            {
+                new Solve.Node(9, new List<Solve.Node>(13)),
+                new Solve.Node(10)
+            }),
+        });
+        
+        Assert.AreEqual(new List<int>() {1,2,3,6,7,11,14,4,8,12,5,9,13,10}, solve.Preorder(tree2), "Example 2");
+    }
+}
