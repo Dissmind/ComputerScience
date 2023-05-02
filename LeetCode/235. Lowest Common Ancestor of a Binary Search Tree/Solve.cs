211dbf06a384aad300e444161a025da7e1280a36
DISSMIND\DissM <dissmind@gmail.com>
2023-05-02 23:16:03 +0200

    [ADD](Leetcode): 235. Lowest Common Ancestor of a Binary Search Tree (in progress)

---
+++ LeetCode/235. Lowest Common Ancestor of a Binary Search Tree/Solve.cs
@@ -0,0 +1,29 @@
+﻿namespace Leetcode.N235LowestCommonAncestorofaBinarySearchTree;
+
+public class Solve
+{ 
+    public class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+    
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+    {
+        if (root.val > p.val && root.val > q.val)
+        {
+            LowestCommonAncestor(root.left, p, q);
+        }
+        else if (root.val > p.val && root.val > q.val)
+        {
+            LowestCommonAncestor(root.right, p, q);
+        }
+        else
+        {
+            return root;
+        }
+
+        return root;
+    }
+}
